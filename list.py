e0fd6c40d3f89deb098cc5f4a451f7b3747f36e8
dalmat-shabani <dalmat.shabani90@gmail.com>
2025-06-05 19:03:13 +0200

    first module

---
+++ list.py
@@ -0,0 +1,46 @@
+shopping_list = ["apples", "bananas", 3,4,5]
+print(shopping_list)
+
+to_do_list = ["math_homework" , "research for biology", "clean the room", "fun"]
+
+
+first_item = to_do_list[0]
+second_item = to_do_list[1]
+third_item = to_do_list[2]
+fourth_item = to_do_list[3]
+
+print(first_item)
+print(second_item)
+print(third_item)
+print(fourth_item)
+
+
+
+
+
+to_do_list.append("teaching")
+print(to_do_list)
+
+shopping_list.insert(2, "oranges")
+print(shopping_list)
+
+
+
+
+
+
+to_do_list.remove("research for biology")
+print(to_do_list)
+
+del to_do_list[2]
+print(to_do_list)
+
+
+
+
+
+
+
+to_do_list[0] = "physics homework"
+print(to_do_list)
+print(to_do_list)
