f98f867fb02dbf52fb19f5138ff31192857ffff5
dalmat-shabani <dalmat.shabani90@gmail.com>
2025-06-12 19:17:32 +0200

    third module

--- Module3/challenge.py
+++ Module3/challenge.py
@@ -3,3 +3,12 @@ loyalty_set = {"James", "arthur", "Steph"}
 loyal = "Steph"
 
 print(loyal in loyalty_set)
+
+my_list = ["bananas", "apples", "oranges"]
+
+my_touple = (1,2,3)
+
+
+print(my_list)
+print(my_touple)
+print(loyalty_set)
