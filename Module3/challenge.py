390e31ccb6951c99434c46a020495d12698e5394
dalmat-shabani <dalmat.shabani90@gmail.com>
2025-06-17 18:28:53 +0200

    third module

--- Module3/challenge.py
+++ Module3/challenge.py
@@ -12,3 +12,17 @@ my_touple = (1,2,3)
 print(my_list)
 print(my_touple)
 print(loyalty_set)
+
+
+
+student_gpa = 4.0
+student_test = 95
+
+if student_gpa >= 3.6 and student_test > 65 :
+    print("Full scolarship!!")
+elif student_gpa >=3.5 and  50 <= student_test <= 65:
+    print("Parital scholarship")
+elif student_gpa >=3.5 and student_test < 50 :
+    print("Not eligible for a scholarship")
+else:
+    print("Not eligible for a scholarship")
---
+++ Module3/colnditionals.py
@@ -0,0 +1,18 @@
+age = 38
+
+if age >= 18:
+    print("You can vote")
+else:
+    print("You cannot vote yet.")
+
+
+
+
+temperature = 28
+
+if temperature > 30:
+    print("Its a hot day, stay hydrated.")
+elif 20 <= temperature <=30:
+    print("the weather is pleasent.")
+else:
+    print("Its a cold day")
