ed5b44f2c29947045f82a6aaa80db199b3ad29ca
dalma <dalmatithegoat@gmail.com>
2025-11-13 19:11:13 +0100

    9

---
+++ module_20/fileJson.py
@@ -0,0 +1,21 @@
+{
+    "name": "John",
+    "age": 30,
+    "address": {
+        "street": "123 Main Street",
+        "city": "New York",
+        "state": "NY",
+        "postcode": "NY12"
+    },
+    "contacts": [
+        {
+            "type": "email",
+            "value": "john@gmail.com"
+        },
+        {
+            "type": "phone",
+            "value": "555-555-5555"
+        }
+    ]
+}
+
